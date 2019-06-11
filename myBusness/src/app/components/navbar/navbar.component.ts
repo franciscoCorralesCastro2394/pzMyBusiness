756df8bbc3420c9847cc9fc7599c845ea26b9e86
francisco <chikocorrales@hotmail.com>
2019-06-10 23:08:28 -0600

    cambios en navbar

--- myBusness/src/app/components/navbar/navbar.component.ts
+++ myBusness/src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { LoginService } from '../../services/loginSeguro/login.service';
 import { DataStorageService } from '../../services/data-storage.service';
-
+import { Observable } from 'rxjs';
+import { UsuariosService } from '../../services/usuarios.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,13 +11,31 @@ import { DataStorageService } from '../../services/data-storage.service';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
+  users$:Observable<any>;
+  usuario_actual$:Observable<any>;
   constructor(
               private router:Router,
               private login:LoginService,
-              private dataStorageService:DataStorageService) { }
+              private dataStorageService:DataStorageService,
+              private usuariosService:UsuariosService) { 
+              this.getUsuario();
+              }
   ngOnInit() {
+    setInterval(this.setUsuarios,1000);
   }
+
+  setUsuarios = () => {
+    this.usuario_actual$ = this.dataStorageService.getObjectValue("UserNow");
+  }
+
+  getUsuario = () => {
+    this.users$ = this.usuariosService.getAllUaurios();
+    this.usuario_actual$ = this.dataStorageService.getObjectValue("UserNow");
+  }
+
+  logueado(email:String){
+    this.router.navigate(['/private/informacion-usuario/'+email]);
+  } 
 
   LoginOff(){
       this.login.logout();
